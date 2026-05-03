b292b2b86432f427fce08e6ce870f525e3b180f8
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-05-03 15:31:36 +0530

    config server client and config server enabled

--- finflow-be/config-server/src/main/java/com/finflow/finflow/FinflowApplication.java
+++ finflow-be/config-server/src/main/java/com/finflow/finflow/FinflowApplication.java
@@ -2,7 +2,9 @@ package com.finflow.finflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class FinflowApplication {
 
