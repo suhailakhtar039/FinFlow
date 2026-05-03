d7c2cd3c3e68470ffb87d23b9409551830a8eebb
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-05-03 12:22:19 +0530

    create nine microservices

---
+++ finflow-be/config-server/src/test/java/com/finflow/finflow/FinflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finflow.finflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
