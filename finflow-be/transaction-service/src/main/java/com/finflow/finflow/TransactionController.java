7b1c2f59c6073a820ba2eca4b6ba971454e01a8f
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-05-07 23:12:23 +0530

    TransactionController created

---
+++ finflow-be/transaction-service/src/main/java/com/finflow/finflow/TransactionController.java
@@ -0,0 +1,19 @@
+package com.finflow.finflow;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/transactions")
+@RequiredArgsConstructor
+public class TransactionController {
+    private final TransactionProducer transactionProducer;
+    @PostMapping
+    public String create(@RequestBody String message){
+        transactionProducer.send(message);
+        return "Transaction event sent";
+    }
+}
