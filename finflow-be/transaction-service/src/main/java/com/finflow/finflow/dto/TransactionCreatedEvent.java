58bbbb0fb966ad762f4ee816290c8ab2093510ca
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-05-13 22:34:30 +0530

    create one DTO

---
+++ finflow-be/transaction-service/src/main/java/com/finflow/finflow/dto/TransactionCreatedEvent.java
@@ -0,0 +1,21 @@
+package com.finflow.finflow.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TransactionCreatedEvent {
+    private String transactionId;
+    private String senderId;
+    private String receiverId;
+    private BigDecimal amount;
+    private LocalDateTime createdAt;
+}
