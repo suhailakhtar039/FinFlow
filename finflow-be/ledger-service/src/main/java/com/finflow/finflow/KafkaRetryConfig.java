fa3f075040c21f87dc926426db59c7dee4953ed9
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-05-15 21:26:16 +0530

    create this for retry

---
+++ finflow-be/ledger-service/src/main/java/com/finflow/finflow/KafkaRetryConfig.java
@@ -0,0 +1,9 @@
+package com.finflow.finflow;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@Configuration
+@EnableKafka
+public class KafkaRetryConfig {
+}
