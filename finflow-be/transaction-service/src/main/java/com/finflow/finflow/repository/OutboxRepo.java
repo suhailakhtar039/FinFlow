aa9649ee74766e0034af3970f18c7472ecbc800d
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-05-16 12:33:52 +0530

    add @Repository annotation

--- finflow-be/transaction-service/src/main/java/com/finflow/finflow/repository/OutboxRepo.java
+++ finflow-be/transaction-service/src/main/java/com/finflow/finflow/repository/OutboxRepo.java
@@ -2,9 +2,11 @@ package com.finflow.finflow.repository;
 
 import com.finflow.finflow.entity.OutboxEvent;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface OutboxRepo extends JpaRepository<OutboxEvent, String> {
     List<OutboxEvent> findByPublishedFalse();
 }
