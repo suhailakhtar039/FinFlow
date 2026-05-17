2e5d59b224d97c4695c59019c66d61d518207488
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-05-17 13:28:07 +0530

    modify class WalletKafkaConsumer

--- finflow-be/wallet-service/src/main/java/com/finflow/finflow/service/WalletKafkaConsumer.java
+++ finflow-be/wallet-service/src/main/java/com/finflow/finflow/service/WalletKafkaConsumer.java
@@ -1,12 +1,16 @@
 package com.finflow.finflow.service;
 
 import com.finflow.dto.TransactionCreatedEvent;
+import com.finflow.dto.WalletDebitedEvent;
 import com.finflow.finflow.entity.Wallet;
 import com.finflow.finflow.repository.WalletRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -14,19 +18,60 @@ public class WalletKafkaConsumer {
 
     private final WalletRepo walletRepo;
 
-    @KafkaListener(topics = "transaction-topic", groupId = "wallet-group")
+    private final KafkaTemplate<String, Object>
+            kafkaTemplate;
+
+    @KafkaListener(
+            topics = "transaction-topic",
+            groupId = "wallet-group"
+    )
     @Transactional
-    public void consume(TransactionCreatedEvent event) {
-        Wallet sender = walletRepo
-                .findById(event.getSenderId())
-                .orElseThrow();
-        if (sender.getBalance().compareTo(event.getAmount()) < 0) {
-            throw new RuntimeException("Insufficient Balance");
+    public void consume(
+            TransactionCreatedEvent transaction
+    ) {
+
+        Wallet sender =
+                walletRepo.findById(
+                        transaction.getSenderId()
+                ).orElseThrow();
+
+        if (sender.getBalance().compareTo(
+                transaction.getAmount()) < 0) {
+
+            throw new RuntimeException(
+                    "Insufficient balance"
+            );
         }
-        sender.setBalance(sender.getBalance().subtract(event.getAmount()));
+
+        sender.setBalance(
+                sender.getBalance()
+                        .subtract(transaction.getAmount())
+        );
 
         walletRepo.save(sender);
 
-        System.out.println("Wallet Debited Successfully");
+        // STEP 3 HERE
+        WalletDebitedEvent event =
+                WalletDebitedEvent.builder()
+                        .transactionId(
+                                transaction.getTransactionId()
+                        )
+                        .senderId(
+                                transaction.getSenderId()
+                        )
+                        .amount(
+                                transaction.getAmount()
+                        )
+                        .createdAt(LocalDateTime.now())
+                        .build();
+
+        kafkaTemplate.send(
+                "wallet-debited-topic",
+                event
+        );
+
+        System.out.println(
+                "Wallet debited successfully"
+        );
     }
 }
