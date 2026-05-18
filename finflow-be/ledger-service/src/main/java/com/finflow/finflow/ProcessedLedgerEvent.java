fe109280905835d156e4b4ff58d6bcc009c83e44
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-05-18 22:41:51 +0530

    ProcessedLedgerEvent working along with its repo

---
+++ finflow-be/ledger-service/src/main/java/com/finflow/finflow/ProcessedLedgerEvent.java
@@ -0,0 +1,19 @@
+package com.finflow.finflow;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "processed_ledger_events")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProcessedLedgerEvent {
+
+    @Id
+    private String transactionId;
+}
