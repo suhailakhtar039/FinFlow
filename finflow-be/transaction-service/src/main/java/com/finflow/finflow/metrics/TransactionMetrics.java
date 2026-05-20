13576a983b8a317761c89a7236e21448b3508ed3
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-05-20 22:51:30 +0530

    create TransactionMetrics

---
+++ finflow-be/transaction-service/src/main/java/com/finflow/finflow/metrics/TransactionMetrics.java
@@ -0,0 +1,41 @@
+package com.finflow.finflow.metrics;
+
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TransactionMetrics {
+
+    private final Counter transactionSuccessCounter;
+    private final Counter transactionFailureCounter;
+    private final Counter transactionCompensationCounter;
+
+    public TransactionMetrics(MeterRegistry meterRegistry) {
+        this.transactionSuccessCounter =
+                Counter.builder("transactions_completed_total")
+                        .description("Total completed transactions")
+                        .register(meterRegistry);
+        this.transactionFailureCounter =
+                Counter.builder("transactions_failed_total")
+                        .description("Total failed transactions")
+                        .register(meterRegistry);
+        this.transactionCompensationCounter =
+                Counter.builder("transactions_compensated_total")
+                        .description("Total compensated transactions")
+                        .register(meterRegistry);
+    }
+
+    public void incrementSuccess() {
+        transactionSuccessCounter.increment();
+    }
+
+    public void incrementFailure() {
+        transactionFailureCounter.increment();
+    }
+
+    public void incrementCompensation() {
+        transactionCompensationCounter.increment();
+    }
+
+}
