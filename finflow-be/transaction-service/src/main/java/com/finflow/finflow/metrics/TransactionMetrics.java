e2e97f758e2074160c378b04c366938e40131eba
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-05-20 22:59:58 +0530

    refactor TransactionMetrics

--- finflow-be/transaction-service/src/main/java/com/finflow/finflow/metrics/TransactionMetrics.java
+++ finflow-be/transaction-service/src/main/java/com/finflow/finflow/metrics/TransactionMetrics.java
@@ -10,19 +10,27 @@ public class TransactionMetrics {
     private final Counter transactionSuccessCounter;
     private final Counter transactionFailureCounter;
     private final Counter transactionCompensationCounter;
+    private final Counter transactionInitiatedCounter;
 
     public TransactionMetrics(MeterRegistry meterRegistry) {
         this.transactionSuccessCounter =
                 Counter.builder("transactions_completed_total")
                         .description("Total completed transactions")
                         .register(meterRegistry);
+
         this.transactionFailureCounter =
                 Counter.builder("transactions_failed_total")
                         .description("Total failed transactions")
                         .register(meterRegistry);
+
         this.transactionCompensationCounter =
                 Counter.builder("transactions_compensated_total")
                         .description("Total compensated transactions")
+                        .register(meterRegistry);
+
+        this.transactionInitiatedCounter =
+                Counter.builder("transactions_initiated_total")
+                        .description("Total initiated transactions")
                         .register(meterRegistry);
     }
 
@@ -36,6 +44,10 @@ public class TransactionMetrics {
 
     public void incrementCompensation() {
         transactionCompensationCounter.increment();
+    }
+
+    public void incrementInitiated(){
+        transactionInitiatedCounter.increment();
     }
 
 }
