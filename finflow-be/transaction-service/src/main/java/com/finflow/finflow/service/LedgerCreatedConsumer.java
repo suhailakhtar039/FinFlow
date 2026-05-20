a1de94037151e3888d8c1e9c9128dfe26b51ed89
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-05-20 22:51:50 +0530

    create LedgerCreatedConsumer

--- finflow-be/transaction-service/src/main/java/com/finflow/finflow/service/LedgerCreatedConsumer.java
+++ finflow-be/transaction-service/src/main/java/com/finflow/finflow/service/LedgerCreatedConsumer.java
@@ -3,17 +3,21 @@ package com.finflow.finflow.service;
 import com.finflow.dto.LedgerCreatedEvent;
 import com.finflow.dto.TransactionStatus;
 import com.finflow.finflow.entity.Transaction;
+import com.finflow.finflow.metrics.TransactionMetrics;
 import com.finflow.finflow.repository.TransactionRepo;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class LedgerCreatedConsumer {
 
     private final TransactionRepo transactionRepo;
+    private final TransactionMetrics transactionMetrics;
 
     @KafkaListener(
             topics = "ledger-created-topic",
@@ -35,8 +39,11 @@ public class LedgerCreatedConsumer {
 
         transactionRepo.save(transaction);
 
-        System.out.println(
-                "Transaction COMPLETED"
+        transactionMetrics.incrementSuccess();
+
+        log.info(
+                "Transaction COMPLETED for transactionId={}",
+                event.getTransactionId()
         );
     }
 }
