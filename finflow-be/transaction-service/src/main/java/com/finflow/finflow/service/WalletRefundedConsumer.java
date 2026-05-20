14c358867853bb10d6a146d22158f00e562fb06f
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-05-20 22:52:18 +0530

    create WalletRefundedConsumer

--- finflow-be/transaction-service/src/main/java/com/finflow/finflow/service/WalletRefundedConsumer.java
+++ finflow-be/transaction-service/src/main/java/com/finflow/finflow/service/WalletRefundedConsumer.java
@@ -3,15 +3,19 @@ package com.finflow.finflow.service;
 import com.finflow.dto.TransactionStatus;
 import com.finflow.dto.WalletRefundedEvent;
 import com.finflow.finflow.entity.Transaction;
+import com.finflow.finflow.metrics.TransactionMetrics;
 import com.finflow.finflow.repository.TransactionRepo;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class WalletRefundedConsumer {
     private final TransactionRepo transactionRepo;
+    private final TransactionMetrics transactionMetrics;
 
     @KafkaListener(topics = "wallet-refunded-topic", groupId = "transaction-group")
     public void consume(WalletRefundedEvent event){
@@ -21,7 +25,7 @@ public class WalletRefundedConsumer {
 
         transaction.setStatus(TransactionStatus.REVERSED);
         transactionRepo.save(transaction);
-
-        System.out.println("============== TRANS REVERSED===========");
+        transactionMetrics.incrementCompensation();
+        log.info("========Trans Reversed===========");
     }
 }
